33b30186d36949655cf69a3282a64c18e8839a55
Pradeep A <sharmapradeep872@gmail.com>
2021-11-16 00:23:51 +0530

    fix return error

--- pages/api/feedback/[siteId].js
+++ pages/api/feedback/[siteId].js
@@ -5,6 +5,5 @@ export default async function handler(req, res) {
   if (error) {
     return res.status(500).json({ error });
   }
-  // res.status(200).json({ feedback });
-  res.status(500).json({ error });
+  res.status(200).json({ feedback });
 }
