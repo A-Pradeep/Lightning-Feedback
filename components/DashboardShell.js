f8f43c611015cca5b030b47acac90756480d143c
Pradeep A <sharmapradeep872@gmail.com>
2021-11-16 00:11:33 +0530

    Updated Shell

--- components/DashboardShell.js
+++ components/DashboardShell.js
@@ -1,13 +1,5 @@
 import Navbar from "./navbar";
-import {
-  useColorMode,
-  Flex,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  Heading,
-} from "@chakra-ui/react";
-import AddSiteModal from "./AddSiteModal";
+import { useColorMode, Flex } from "@chakra-ui/react";
 
 function DashboardShell({ children, siteCount }) {
   const { colorMode } = useColorMode();
@@ -17,18 +9,8 @@ function DashboardShell({ children, siteCount }) {
       <Flex
         backgroundColor={colorMode === "light" ? "gray.100" : "gray.700"}
         p={8}
-        height="100vh"
       >
         <Flex w="100%" mx="auto" direction="column" maxWidth="80%">
-          <Breadcrumb>
-            <BreadcrumbItem isCurrentPage>
-              <BreadcrumbLink>Sites</BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-          <Flex justifyContent="space-between">
-            <Heading mb={4}>My Sites</Heading>
-            {siteCount > 0 && <AddSiteModal> + Add Site</AddSiteModal>}
-          </Flex>
           {children}
         </Flex>
       </Flex>
